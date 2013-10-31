c7315dfa1117bafcb193d4fbd9cdaa27d08e932d
Roberto E. Vargas Caballero <k0ga@shike2.com>
2013-10-31 21:13:19 +0100

    Fix struct field declaration
    struct fields cannot have storage or qualifiers specifiers, and
    it causes they can be defined without explicit type.

--- decl.c
+++ decl.c
@@ -220,12 +220,15 @@ specifier(register struct ctype *tp,
 			if (!tp->defined) {
 				if (!store->defined &&
 				    !qlf->defined   &&
-				    curctx != CTX_OUTER ) {
+				    curctx != CTX_OUTER &&
+				    nested_tags == 0) {
 					return false;
 				}
 				warn(options.implicit,
 				     "type defaults to 'int' in declaration");
 			}
+			if (nested_tags > 0 && (qlf->defined || store->defined))
+				error("type qualifer or store specifier in field declaration");
 			if (!tp->c_signed && !tp->c_unsigned) {
 				switch (tp->type) {
 				case CHAR:
