eff46e86a3d7b4106d97977bd974c7266ecc9c6c
FRIGN <dev@frign.de>
2015-07-19 20:04:08 +0200

    Update config.mk reflecting necessities and remove shell-scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-. ./env.sh
-make "$@"
--- config.mk
+++ config.mk
@@ -1,15 +1,17 @@
-# Customize to fit your system
-
-# paths
-PREFIX      = /usr/local/
-MANPREFIX   = ${PREFIX}/share/man
-
+# scc version
 VERSION     = 0.1
 
-# Flags
-CFLAGS      = -std=c99
-#LDFLAGS     =
+# Customize below to fit your system
 
-# Tools
-CC          = cc
-#AR          =
+# paths
+PREFIX    = /usr/local/
+MANPREFIX = ${PREFIX}/share/man
+
+CC = cc
+LD = $(CC)
+AR = ar
+
+# for Plan9 add -D_SUSV2_SOURCE -DNBOOL
+CPPFLAGS = -D_DEFAULT_SOURCE
+CFLAGS   = -std=c99
+LDFLAGS  = -s
