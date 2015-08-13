3e1a3035dc3e9959cdbba2cc3d658eacbf62d761
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-08-13 23:48:07 +0200

    Add test002
    This test tests forward references of structures.
    At this moment it is failling, but it is a first
    step to fix it.

---
+++ cc1/tests/test002.c
@@ -0,0 +1,32 @@
+/*
+name: TEST002
+description: Test forward references before definition of type definition
+output:
+S2	S
+G4	P	x
+F1
+X6	F1	main
+G6	F1	main	{
+-
+S2	S	(
+M5	I
+)
+A2	S2	y
+	A2	M5	.I	#I0	:I
+	G4	@S2	A2	:S
+}
+*/
+
+struct S *x;
+struct S {
+	int i;
+};
+
+void
+main(void)
+{
+	struct S y;
+
+	y.i = 0;
+	*x = y;
+}
