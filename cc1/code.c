5fb1525449fa2103cadc946ff467dec95a3da638
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-08-14 17:23:56 +0200

    Avoid print type letters in emitbin()
    If there is no letter for the operator it means that is a non
    sense to print the type of the node.

--- cc1/code.c
+++ cc1/code.c
@@ -307,9 +307,10 @@ emitbin(unsigned op, void *arg)
 
 	emitnode(np->left);
 	emitnode(np->right);
-	if ((s = optxt[op]) != NULL)        /* do not print in OCOLON case */
+	if ((s = optxt[op]) != NULL)  {      /* do not print in OCOLON case */
 		printf("\t%s", optxt[op]);
-	emitletter(np->type);
+		emitletter(np->type);
+	}
 }
 
 static void
