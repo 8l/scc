5c3dd68b9c8862b5eee0f9b9f08928c702e7c607
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-08-15 20:40:40 +0200

    Add basic test for for loops

---
+++ cc1/tests/test009.c
@@ -0,0 +1,37 @@
+/*
+name: TEST009
+description: Basic test for loops
+output:
+F1
+G1	F1	main
+{
+-
+A2	I	x
+	A2	#I0	:I
+	j	L5
+	d
+L3
+	A2	A2	#I1	+I	:I
+L5
+	j	L3	A2	#IA	<I
+	b
+L4
+	j	L6	A2	#IA	=I
+	yI	#I1
+L6
+	yI	#I0
+}
+*/
+
+int 
+main()
+{
+	int x;
+	
+	for (x = 0; x < 10 ; x = x + 1)
+		;
+	if (x != 10)
+		return 1;
+	return 0;
+}
+
