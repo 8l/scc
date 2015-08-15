ec647d24eb10e22e3f54acc0e22c9a9b91a2b6f7
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-08-15 20:09:02 +0200

    Add basic test for do while statements

---
+++ cc1/tests/test008.c
@@ -0,0 +1,43 @@
+/*
+name: TEST008
+description: Basic do while loop
+output:
+F1
+G1	F1	main
+{
+-
+A2	I	x
+	A2	#I0	:I
+	d
+L3
+	A2	A2	#I1	+I	:I
+	j	L3	A2	#IA	<I
+	b
+L4
+	d
+L5
+	A2	A2	#I1	+I	:I
+	j	L5	A2	#I14	<I
+	b
+L6
+	yI	A2	#I14	-I
+}
+*/
+
+int 
+main()
+{
+	int x;
+	
+	x = 0;
+	do 
+	  x = x + 1;
+	while(x < 10);
+	
+	do {
+	  x = x + 1;
+	} while(x < 20);
+	
+	return x - 20;
+}
+
