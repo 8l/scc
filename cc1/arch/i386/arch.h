0ba1212867ef18e71a4f05ed19b6d0c89208e954
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-09-25 17:48:15 +0200

    Add support for i386 in cc1
    We only need to configure the types, and the rest of the code
    will work without problems.

---
+++ cc1/arch/i386/arch.h
@@ -0,0 +1,21 @@
+
+#define TINT        long long
+#define TUINT       unsigned long long
+#define TFLOAT      double
+
+#define L_SCHAR     L_INT8
+#define L_UCHAR     L_UINT8
+#define L_CHAR      L_INT8
+#define L_SHORT     L_INT16
+#define L_USHORT    L_UINT16
+#define L_INT       L_INT32
+#define L_UINT      L_UINT32
+#define L_LONG      L_INT32
+#define L_ULONG     L_UINT32
+#define L_LLONG     L_INT64
+#define L_ULLONG    L_UINT64
+#define L_BOOL      'B'
+#define L_FLOAT     'J'
+#define L_DOUBLE    'D'
+#define L_LDOUBLE   'H'
+#define L_ENUM      L_INT
