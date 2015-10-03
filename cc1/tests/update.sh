fa0027223d9664bec5f475be753b5f6258584cb5
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-10-03 12:06:01 +0200

    Add optional parameter to update.sh
    This tool is good when you want to update only one test,
    so the previous behaviour of updating all the tests
    was not always the desired result.

--- cc1/tests/update.sh
+++ cc1/tests/update.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
+update()
+{
+	(echo '/^output/+;/^\*\//-c'
+	../cc1 -I./ -w $1 2>&1
+	printf ".\nw\n") | ed -s $1
+}
+
+
+case $# in
+1)
+	update $1
+	exit
+	;;
+*)
+	echo "usage: update.sh [test]" >&2
+	exit 1
+	;;
+esac
+
+
 for i in *.c
 do
-	(echo '/^output/+;/^\*\//-c'
-	../cc1 -w $i 2>&1
-	printf ".\nw\n") | ed -s $i
+	update $i
 done
