06ccd3a0b986f0eb634bf642fe69ba9434a39ed2
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-10-06 15:26:33 +0200

    Add test for Duff device

---
+++ cc1/tests/test036.c
@@ -0,0 +1,32 @@
+
+/*
+name: TEST036
+description: Duff's device
+output:
+
+*/
+
+/* Disgusting, no?  But it compiles and runs just fine.  I feel a combination of
+   pride and revulsion at this discovery.  If no one's thought of it before,
+   I think I'll name it after myself.
+   It amazes me that after 10 years of writing C there are still
+   little corners that I haven't explored fully.
+   - Tom Duff */
+
+send(to, from, count)
+	register short *to, *from;
+	register count;
+{
+	register n=(count+7)/8;
+	switch(count%8){
+	case 0:      do{*to = *from++;
+	case 7:           *to = *from++;
+	case 6:           *to = *from++;
+	case 5:           *to = *from++;
+	case 4:           *to = *from++;
+	case 3:           *to = *from++;
+	case 2:           *to = *from++;
+	case 1:           *to = *from++;
+	                    }while(--n>0);
+	}
+}
