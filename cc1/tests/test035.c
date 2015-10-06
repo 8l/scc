fbc4a2c2bfb90654aafbdaf3c3e2a76b807ea19b
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-10-06 15:21:30 +0200

    Add basic test for enumerations

---
+++ cc1/tests/test035.c
@@ -0,0 +1,30 @@
+
+/*
+name: TEST035
+description: Basic test for enumerations
+output:
+
+*/
+
+enum E {
+	x,
+	y = 2,
+	z,
+};
+
+
+int
+main()
+{
+	enum E e;
+
+	if(x != 0)
+		return 1;
+	if(y != 2)
+		return 2;
+	if(z != 3)
+		return 3;
+	
+	e = x;
+	return x;
+}
