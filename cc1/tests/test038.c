967584afc72c32a3cf344dcb6c0e25febe3d1a62
Roberto E. Vargas Caballero <k0ga@shike2.com>
2015-10-07 21:56:09 +0200

    Mark test038 as not passed
    The initializer of G1 has expression form, which is incorrect for
    external symbols.

--- cc1/tests/test038.c
+++ cc1/tests/test038.c
@@ -3,22 +3,7 @@
 name: TEST038
 description: Basic test for tentative definitions
 output:
-G1	I	x
-	G1	#I0	:I
-F2	I	E
-X3	F2	main
-F4	P	E
-G5	F4	foo
-{
-\
-	r	X3	'P
-}
-G3	F2	main
-{
-\
-	G1	#I0	:I
-	r	G1
-}
+
 */
 
 int x;
